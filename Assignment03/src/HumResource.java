ce4952f41aae5930e02bff0badb41bd62cc583ab
kohlsjw3656 <kohlsjw3656@gmail.com>
2019-11-13 15:06:29 -0600

    Added spaces to commas

--- Assignment03/src/HumResource.java
+++ Assignment03/src/HumResource.java
@@ -10,7 +10,7 @@ public abstract class HumResource {
     String where = "WHERE ";
     String values = "VALUES (";
     String hridEquals = "hrid = ";
-    String comma = ",";
+    String comma = ", ";
     int hrid;
 
 
