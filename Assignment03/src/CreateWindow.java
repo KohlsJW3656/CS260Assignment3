40e574e9a7e823037899d7d70d553c162ade999e
Dylan Black <blacktdylan@gmail.com>
2019-11-12 19:51:44 -0600

    insert gui screen

--- Assignment03/src/CreateWindow.java
+++ Assignment03/src/CreateWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -45,12 +46,95 @@ public class CreateWindow {
         JFrame newFrame = new JFrame("New Window");
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         newFrame.setSize(300,300);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        JLabel label = new JLabel("Please enter the information");
+        JButton submit = new JButton("Submit");
 
-        JLabel label = new JLabel(operation);
-        JLabel label2 = new JLabel(resource);
-        newFrame.getContentPane().add(BorderLayout.NORTH, label);
-        newFrame.getContentPane().add(BorderLayout.SOUTH, label2);
+        if(resource.equals("Water")) {
+            if(operation.equals("Insert")){
+                JPanel input = new JPanel();
+                JTextField bottles = new JTextField("Number of 10oz bottles");
+                JTextField halfLiterBottles = new JTextField("Number of half-liter bottles");
+                JTextField jugs = new JTextField("Number of 5gal jugs");
 
+                input.add(bottles);
+                input.add(halfLiterBottles);
+                input.add(jugs);
+
+                panel.add(input, BorderLayout.CENTER);
+
+                // Water water = new Water();
+                //water.insert();
+            }
+            else if(operation.equals("Update")){
+                Water water = new Water();
+                water.update();
+            }
+            else {
+                Water water = new Water();
+                water.delete();
+            }
+        }
+
+        else if(resource.equals("Food")) {
+            if(operation.equals("Insert")){
+                JPanel input = new JPanel();
+                JTextField type = new JTextField("Food type");
+                JTextField meals = new JTextField("Number of meals available");
+                JTextField desc = new JTextField("Description");
+
+                input.add(type);
+                input.add(meals);
+                input.add(desc);
+
+                panel.add(input, BorderLayout.CENTER);
+
+
+                //Food food = new Food();
+                //food.insert();
+            }
+            else if(operation.equals("Update")){
+                Food food = new Food();
+                food.update();
+            }
+            else {
+                Food food = new Food();
+                food.delete();
+            }
+        }
+
+        else {
+            if(operation.equals("Insert")){
+                JPanel input = new JPanel();
+                JTextField beds = new JTextField("Number of beds");
+                JTextField roomCap = new JTextField("Emergency room capacity");
+                JTextField doctors = new JTextField("Number of doctors");
+                JTextField nurses = new JTextField("Number of nurses");
+
+                input.add(beds);
+                input.add(roomCap);
+                input.add(doctors);
+                input.add(nurses);
+
+                panel.add(input, BorderLayout.CENTER);
+
+                //MedicalCenter medicalCenter = new MedicalCenter();
+                //medicalCenter.insert();
+            }
+            else if(operation.equals("Update")){
+                MedicalCenter medicalCenter = new MedicalCenter();
+                medicalCenter.update();
+            }
+            else {
+                MedicalCenter medicalCenter = new MedicalCenter();
+                medicalCenter.delete();
+            }
+        }
+
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(submit, BorderLayout.SOUTH);
+        newFrame.getContentPane().add(panel);
         newFrame.setVisible(true);
     }
 }
