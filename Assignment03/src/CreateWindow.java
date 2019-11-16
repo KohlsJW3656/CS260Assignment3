618af2d33acbc9822fcaef4b3a013d8ae432297f
Dylan Black <blacktdylan@gmail.com>
2019-11-16 11:21:43 -0600

    Updated GUI

--- Assignment03/src/CreateWindow.java
+++ Assignment03/src/CreateWindow.java
@@ -82,17 +82,44 @@ public class CreateWindow {
                         int numOf5Gal = Integer.parseInt(jugs.getText());
 
                         Water water = new Water();
-                        water.insert(-1,numOf10Bottles, numOfHalfLitter, numOf5Gal);
+                        water.insert(hrId, numOf10Bottles, numOfHalfLitter, numOf5Gal);
                     }
                 });
             }
             else if(operation.equals("Update")){
-                Water water = new Water();
-                //water.update();
+                JPanel input = new JPanel(new GridLayout(3,2));
+                JLabel label1 = new JLabel("Number of 10oz bottles:");
+                JLabel label2 = new JLabel("Number of half-liter bottles:");
+                JLabel label3 = new JLabel("Number of 5gal jugs:");
+                JTextField bottles = new JTextField();
+                JTextField halfLiterBottles = new JTextField();
+                JTextField jugs = new JTextField();
+
+                input.add(label1);
+                input.add(bottles);
+
+                input.add(label2);
+                input.add(halfLiterBottles);
+
+                input.add(label3);
+                input.add(jugs);
+
+                panel.add(input, BorderLayout.CENTER);
+
+                submit.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent ae) {
+                        int numOf10Bottles = Integer.parseInt(bottles.getText());
+                        int numOfHalfLitter = Integer.parseInt(halfLiterBottles.getText());
+                        int numOf5Gal = Integer.parseInt(jugs.getText());
+
+                        Water water = new Water();
+                        water.update(hrId, numOf10Bottles, numOfHalfLitter, numOf5Gal);
+                    }
+                });
             }
             else {
                 Water water = new Water();
-                //water.delete();
+                water.delete(hrId);
             }
         }
 
@@ -125,21 +152,45 @@ public class CreateWindow {
                         String description = desc.getText();
 
                         Food food = new Food();
-                        food.insert(-1, typeOfFood, numOfMeals, description);
+                        food.insert(hrId, typeOfFood, numOfMeals, description);
                     }
                 });
-
-
-                //Food food = new Food();
-                //food.insert();
             }
             else if(operation.equals("Update")){
-                //Food food = new Food();
-                //food.update();
+                JPanel input = new JPanel(new GridLayout(3,2));
+                JLabel label1 = new JLabel("Food type:");
+                JLabel label2 = new JLabel("Number of meals available:");
+                JLabel label3 = new JLabel("Description");
+
+                JTextField type = new JTextField();
+                JTextField meals = new JTextField();
+                JTextField desc = new JTextField();
+
+                input.add(label1);
+                input.add(type);
+
+                input.add(label2);
+                input.add(meals);
+
+                input.add(label3);
+                input.add(desc);
+
+                panel.add(input, BorderLayout.CENTER);
+
+                submit.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent ae) {
+                        String typeOfFood = type.getText();
+                        int numOfMeals = Integer.parseInt(meals.getText());
+                        String description = desc.getText();
+
+                        Food food = new Food();
+                        food.update(hrId, typeOfFood, numOfMeals, description);
+                    }
+                });
             }
             else {
-                //Food food = new Food();
-                //food.delete();
+                Food food = new Food();
+                food.delete(hrId);
             }
         }
 
@@ -178,17 +229,51 @@ public class CreateWindow {
                         int numOfNurses = Integer.parseInt(nurses.getText());
 
                         MedicalCenter medicalCenter = new MedicalCenter();
-                        medicalCenter.insert(-1, numOfBeds, roomCapacity, numOfdoctors, numOfNurses);
+                        medicalCenter.insert(hrId, numOfBeds, roomCapacity, numOfdoctors, numOfNurses);
                     }
                 });
             }
             else if(operation.equals("Update")){
-                MedicalCenter medicalCenter = new MedicalCenter();
-                //medicalCenter.update();
+                JPanel input = new JPanel(new GridLayout(4,2));
+                JLabel label1 = new JLabel("Number of beds");
+                JLabel label2 = new JLabel("Emergency room capacity");
+                JLabel label3 = new JLabel("Number of doctors");
+                JLabel label4 = new JLabel("Number of nurses");
+
+                JTextField beds = new JTextField("", 5);
+                JTextField roomCap = new JTextField();
+                JTextField doctors = new JTextField();
+                JTextField nurses = new JTextField();
+
+                input.add(label1);
+                input.add(beds);
+
+                input.add(label2);
+                input.add(roomCap);
+
+                input.add(label3);
+                input.add(doctors);
+
+                input.add(label4);
+                input.add(nurses);
+
+                panel.add(input, BorderLayout.CENTER);
+
+                submit.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent ae) {
+                        int numOfBeds = Integer.parseInt(beds.getText());
+                        int roomCapacity = Integer.parseInt(roomCap.getText());
+                        int numOfdoctors = Integer.parseInt(doctors.getText());
+                        int numOfNurses = Integer.parseInt(nurses.getText());
+
+                        MedicalCenter medicalCenter = new MedicalCenter();
+                        medicalCenter.update(hrId, numOfBeds, roomCapacity, numOfdoctors, numOfNurses);
+                    }
+                });
             }
             else {
                 MedicalCenter medicalCenter = new MedicalCenter();
-                //medicalCenter.delete();
+                medicalCenter.delete(hrId);
             }
         }
 
@@ -256,10 +341,10 @@ public class CreateWindow {
         submit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 HumResource hr = new HumResource();
-                hr.insert(hrName.getText(), hrAddress.getText(), hrPhone.getText(), hrLat.getText(),
+                int hrID = hr.insert(hrName.getText(), hrAddress.getText(), hrPhone.getText(), hrLat.getText(),
                        hrLon.getText(), hrType.getText(), hrDesc.getText(), hrHours.getText());
                 humResFrame.dispose();
-                newWindow(-1, "Insert", resource);
+                newWindow(hrID, "Insert", resource);
             }
         });
 
@@ -267,6 +352,78 @@ public class CreateWindow {
         panel.add(submit, BorderLayout.SOUTH);
         humResFrame.getContentPane().add(panel);
         humResFrame.setVisible(true);
+    }
+
+    private static void updateHRFrame(int hrID, String resource){
+        JFrame updateHRFrame = new JFrame("Humanitarian Resource");
+        updateHRFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        updateHRFrame.setSize(600,400);
+        updateHRFrame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        JLabel label = new JLabel("Please enter the Humanitarian Resource information");
+        JButton submit = new JButton("Submit");
+
+        JPanel input = new JPanel(new GridLayout(8,2));
+        JLabel name = new JLabel("Humanitarian Resource Name");
+        JLabel address = new JLabel("Address");
+        JLabel phone = new JLabel("Phone Number");
+        JLabel lat = new JLabel("Latitude");
+        JLabel lon = new JLabel("Longitude");
+        JLabel type = new JLabel("Type");
+        JLabel desc = new JLabel("Description");
+        JLabel hours = new JLabel("Hours Open");
+
+        JTextField hrName = new JTextField();
+        JTextField hrAddress = new JTextField();
+        JTextField hrPhone = new JTextField();
+        JTextField hrLat = new JTextField();
+        JTextField hrLon = new JTextField();
+        JTextField hrType = new JTextField();
+        JTextField hrDesc = new JTextField();
+        JTextField hrHours = new JTextField();
+
+        input.add(name);
+        input.add(hrName);
+
+        input.add(address);
+        input.add(hrAddress);
+
+        input.add(phone);
+        input.add(hrPhone);
+
+        input.add(lat);
+        input.add(hrLat);
+
+        input.add(lon);
+        input.add(hrLon);
+
+        input.add(type);
+        input.add(hrType);
+
+        input.add(desc);
+        input.add(hrDesc);
+
+        input.add(hours);
+        input.add(hrHours);
+
+        panel.add(input, BorderLayout.CENTER);
+
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                HumResource hr = new HumResource();
+                hr.update(hrID, hrName.getText(), hrAddress.getText(), hrPhone.getText(), hrLat.getText(),
+                        hrLon.getText(), hrType.getText(), hrDesc.getText(), hrHours.getText());
+                updateHRFrame.dispose();
+                newWindow(hrID, "Insert", resource);
+            }
+        });
+
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(submit, BorderLayout.SOUTH);
+        updateHRFrame.getContentPane().add(panel);
+        updateHRFrame.setVisible(true);
+
     }
 
     private static void getIdFrame(String operation, String resource){
@@ -289,7 +446,12 @@ public class CreateWindow {
             public void actionPerformed(ActionEvent ae) {
                 getIdFrame.dispose();
                 int hrID = Integer.parseInt(id.getText());
-                newWindow(hrID, operation, resource);
+                if(operation.equals("Update")){
+                    updateHRFrame(hrID, resource);
+                }
+                else {
+                    newWindow(hrID, operation, resource);
+                }
             }
         });
 
