04eac5af118a0f47afcf170cf1df29a8fa6b1008
Dylan Black <blacktdylan@gmail.com>
2019-11-20 21:13:53 -0600

    Fixed apostophe errors

--- Assignment03/src/CreateWindow.java
+++ Assignment03/src/CreateWindow.java
@@ -66,7 +66,7 @@ public class CreateWindow {
      * @param operation the operation the user is performing
      * @param resource the resource that the user is operating on
      */
-    private static void newWindow(int hrId, String operation, String resource){
+    private static void updateOrInsert(int hrId, String operation, String resource){
         //the frame that will display the window
         JFrame newFrame = new JFrame("Operation");
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -165,15 +165,6 @@ public class CreateWindow {
                     }
                 });
             }
-            //user wants to delete a water
-            else {
-                //a water object that is used to delete a water
-                Water water = new Water();
-                //delete the water
-                water.delete(hrId);
-                //display the ending frame
-                endFrame();
-            }
         }
 
         else if(resource.equals("Food")) {
@@ -203,16 +194,16 @@ public class CreateWindow {
                 submit.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent ae) {
                         newFrame.dispose();
-                        //gets the type of food that the user wants to input
-                        String typeOfFood = type.getText();
                         //gets the number of meals that the user wants to input
                         int numOfMeals = Integer.parseInt(meals.getText());
-                        //gets the description that the user wants to input
-                        String description = desc.getText();
-                        //a food object that is used for inserting food
+                        //make an array with the type of food and food description
+                        String[] values = new String[]{type.getText(), desc.getText()};
+                        //check values for ' and update them with ''
+                        values = checkForApostrophes(values);
+                        //food object used to insert food
                         Food food = new Food();
                         //insert the food item
-                        food.insert(hrId, typeOfFood, numOfMeals, description);
+                        food.insert(hrId, values[0], numOfMeals, values[1]);
                         //display the ending frame
                         endFrame();
                     }
@@ -253,31 +244,22 @@ public class CreateWindow {
                 submit.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent ae) {
                         newFrame.dispose();
-                        //gets the type of food that the user wants to update to
-                        String typeOfFood = type.getText();
-                        //gets the number of meals that the user wants to update to
+                        //gets the number of meals that the user wants to input
                         int numOfMeals = Integer.parseInt(meals.getText());
-                        //gets the description that the user wants to update to
-                        String description = desc.getText();
+                        //make an array with the type of food and food description
+                        String[] values = new String[]{type.getText(), desc.getText()};
+                        //check values for ' and update them with ''
+                        values = checkForApostrophes(values);
 
                         //a food object is used to update food
                         Food food = new Food();
                         //update the food item
-                        food.update(hrId, typeOfFood, numOfMeals, description);
+                        food.update(hrId, values[0], numOfMeals, values[1]);
                         endFrame();
                     }
                 });
             }
-            else {
-                //user wants to delete a food item
-                //a food object that is used to delete a food object
-                Food food = new Food();
-                //delete the food object
-                food.delete(hrId);
-                endFrame();
-            }
         }
-
         else {
             //user wants to insert a medical center
             if(operation.equals("Insert")){
@@ -385,25 +367,11 @@ public class CreateWindow {
                     }
                 });
             }
-            //user wants to delete a medical center
-            else {
-                //a medical center object used to delete a medical center
-                MedicalCenter medicalCenter = new MedicalCenter();
-                //delete the medical center
-                medicalCenter.delete(hrId);
-                endFrame();
-            }
         }
-
-        if(!operation.equals("Delete")){
-            panel.add(label, BorderLayout.NORTH);
-            panel.add(submit, BorderLayout.SOUTH);
-            newFrame.getContentPane().add(panel);
-            newFrame.setVisible(true);
-        }
-        else {
-            newFrame.dispose();
-        }
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(submit, BorderLayout.SOUTH);
+        newFrame.getContentPane().add(panel);
+        newFrame.setVisible(true);
     }
 
     /**
@@ -470,13 +438,17 @@ public class CreateWindow {
         //a method for when the user wants to submit their information
         submit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
+                String[] values = new String[] {hrName.getText(), hrAddress.getText(), hrPhone.getText(),
+                        hrType.getText(), hrDesc.getText(), hrHours.getText()};
+                //check if any of the values the user inserted have a '
+                values = checkForApostrophes(values);
                 //a humanitarian resource object used to insert one
                 HumResource hr = new HumResource();
                 //inserts a humanitarian resource. the value returned is the humanitarian resource's id
-                int hrID = hr.insert(hrName.getText(), hrAddress.getText(), hrPhone.getText(), Double.parseDouble(hrLat.getText()),
-                       Double.parseDouble(hrLon.getText()), hrType.getText(), hrDesc.getText(), hrHours.getText());
+                int hrID = hr.insert(values[0], values[1], values[2], Double.parseDouble(hrLat.getText()),
+                       Double.parseDouble(hrLon.getText()), values[3], values[4], values[5]);
                 humResFrame.dispose();
-                newWindow(hrID, "Insert", resource);
+                updateOrInsert(hrID, "Insert", resource);
             }
         });
 
@@ -484,96 +456,6 @@ public class CreateWindow {
         panel.add(submit, BorderLayout.SOUTH);
         humResFrame.getContentPane().add(panel);
         humResFrame.setVisible(true);
-    }
-
-    /**
-     * The display for updating a humanitarian resource
-     * @param hrID the hrid of the resource to be updated
-     * @param resource the resource the user wants to update
-     */
-    private static void updateHRFrame(int hrID, String resource){
-        //the frame for the display
-        JFrame updateHRFrame = new JFrame("Humanitarian Resource");
-        updateHRFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        updateHRFrame.setSize(600,400);
-        updateHRFrame.setLocationRelativeTo(null);
-
-        //the following are elements of the display
-        JPanel panel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("Please update the Humanitarian Resource information");
-        JButton submit = new JButton("Submit");
-
-        //the following are elements of the display
-        JPanel input = new JPanel(new GridLayout(8,2));
-        JLabel name = new JLabel("Humanitarian Resource Name");
-        JLabel address = new JLabel("Address");
-        JLabel phone = new JLabel("Phone Number");
-        JLabel lat = new JLabel("Latitude");
-        JLabel lon = new JLabel("Longitude");
-        JLabel type = new JLabel("Type");
-        JLabel desc = new JLabel("Description");
-        JLabel hours = new JLabel("Hours Open");
-
-        //a humanitarian resource object used to get the data currently in the DB
-        HumResource humResource = new HumResource();
-        //an array of the current data in the DB for that HR
-        String[] results = humResource.displayHRID("HumResource", hrID);
-
-        //The following are elements of the display. results[x] is data that is
-        //stored in the DB for that attribute
-        JTextField hrName = new JTextField(results[1]);
-        JTextField hrAddress = new JTextField(results[2]);
-        JTextField hrPhone = new JTextField(results[3]);
-        JTextField hrLat = new JTextField(results[4]);
-        JTextField hrLon = new JTextField(results[5]);
-        JTextField hrType = new JTextField(results[6]);
-        JTextField hrDesc = new JTextField(results[7]);
-        JTextField hrHours = new JTextField(results[8]);
-
-        input.add(name);
-        input.add(hrName);
-
-        input.add(address);
-        input.add(hrAddress);
-
-        input.add(phone);
-        input.add(hrPhone);
-
-        input.add(lat);
-        input.add(hrLat);
-
-        input.add(lon);
-        input.add(hrLon);
-
-        input.add(type);
-        input.add(hrType);
-
-        input.add(desc);
-        input.add(hrDesc);
-
-        input.add(hours);
-        input.add(hrHours);
-
-        panel.add(input, BorderLayout.CENTER);
-
-        //a method to handle when the user wants to update the HR
-        submit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                //a humanitarian resource object used to update
-                HumResource hr = new HumResource();
-                //update humanitarian resource
-                hr.update(hrID, hrName.getText(), hrAddress.getText(), hrPhone.getText(), Double.parseDouble(hrLat.getText()),
-                        Double.parseDouble(hrLon.getText()), hrType.getText(), hrDesc.getText(), hrHours.getText());
-                updateHRFrame.dispose();
-                newWindow(hrID, "Update", resource);
-            }
-        });
-
-        panel.add(label, BorderLayout.NORTH);
-        panel.add(submit, BorderLayout.SOUTH);
-        updateHRFrame.getContentPane().add(panel);
-        updateHRFrame.setVisible(true);
-
     }
 
     /**
@@ -592,6 +474,7 @@ public class CreateWindow {
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Please select the " + resource + " HRID");
         JButton submit = new JButton("Submit");
+
         //a call is made to getIDArray. this returns all the possible ids for a specific resource.
         //the ids are added to a dropdown menu for the user to select from.
         JComboBox idDisplay = new JComboBox(getIDArray(resource));
@@ -612,44 +495,139 @@ public class CreateWindow {
                     updateHRFrame(hrID, resource);
                 }
                 else {
-                    //go to the delete frame
-                    newWindow(hrID, operation, resource);
+                    //delete resource
+                    deleteResource(resource, hrID);
                 }
             }
         });
-
         getIdFrame.getContentPane().add(panel);
         getIdFrame.setVisible(true);
-
     }
 
     /**
-     * A helper method to get an array of all the ids for a certain resource.
-     * This is useful because it is used to allow users to only select and id
-     * from the resource that they selected
-     * @param resource the resource the user selected
-     * @return an array of strings with the ids
+     * The display for updating a humanitarian resource
+     * @param hrID the hrid of the resource to be updated
+     * @param resource the resource the user wants to update
      */
-    public static String[] getIDArray(String resource) {
-        if(resource.equals("Medical Center")){
-            //medical center object used to get the ids
+    private static void updateHRFrame(int hrID, String resource) {
+        //the frame for the display
+        JFrame updateHRFrame = new JFrame("Humanitarian Resource");
+        updateHRFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        updateHRFrame.setSize(600, 400);
+        updateHRFrame.setLocationRelativeTo(null);
+
+        //the following are elements of the display
+        JPanel panel = new JPanel(new BorderLayout());
+        JLabel label = new JLabel("Please update the Humanitarian Resource information");
+        JButton submit = new JButton("Submit");
+
+        //the following are elements of the display
+        JPanel input = new JPanel(new GridLayout(8, 2));
+        JLabel name = new JLabel("Humanitarian Resource Name");
+        JLabel address = new JLabel("Address");
+        JLabel phone = new JLabel("Phone Number");
+        JLabel lat = new JLabel("Latitude");
+        JLabel lon = new JLabel("Longitude");
+        JLabel desc = new JLabel("Description");
+        JLabel hours = new JLabel("Hours Open");
+
+        //a humanitarian resource object used to get the data currently in the DB
+        HumResource humResource = new HumResource();
+        //an array of the current data in the DB for that HR
+        String[] results = humResource.displayHRID("HumResource", hrID);
+
+        //The following are elements of the display. results[x] is data that is
+        //stored in the DB for that attribute
+        JTextField hrName = new JTextField(results[1]);
+        JTextField hrAddress = new JTextField(results[2]);
+        JTextField hrPhone = new JTextField(results[3]);
+        JTextField hrLat = new JTextField(results[4]);
+        JTextField hrLon = new JTextField(results[5]);
+        JTextField hrDesc = new JTextField(results[7]);
+        JTextField hrHours = new JTextField(results[8]);
+
+        input.add(name);
+        input.add(hrName);
+
+        input.add(address);
+        input.add(hrAddress);
+
+        input.add(phone);
+        input.add(hrPhone);
+
+        input.add(lat);
+        input.add(hrLat);
+
+        input.add(lon);
+        input.add(hrLon);
+
+        input.add(desc);
+        input.add(hrDesc);
+
+        input.add(hours);
+        input.add(hrHours);
+
+        panel.add(input, BorderLayout.CENTER);
+
+        //a method to handle when the user wants to update the HR
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //a humanitarian resource object used to update
+                HumResource hr = new HumResource();
+
+                //make an array with the values the user entered
+                String[] values = new String[] {hrName.getText(), hrAddress.getText(), hrPhone.getText(),
+                        results[6], hrDesc.getText(), hrHours.getText()};
+
+                //check values for ' and update them with ''
+                values = checkForApostrophes(values);
+                //update humanitarian resource
+                hr.update(hrID, values[0], values[1], values[2], Double.parseDouble(hrLat.getText()),
+                        Double.parseDouble(hrLon.getText()), values[3], values[4], values[5]);
+                updateHRFrame.dispose();
+                updateOrInsert(hrID, "Update", resource);
+            }
+        });
+
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(submit, BorderLayout.SOUTH);
+        updateHRFrame.getContentPane().add(panel);
+        updateHRFrame.setVisible(true);
+    }
+
+    /**
+     * A method to delete a resource
+     * @param resource the type of resource to be deleted
+     * @param hrId the hrId of the resource to be deleted
+     */
+    private static void deleteResource(String resource, int hrId){
+        if(resource.equals("Medical Center")) {
+            //user wants to delete a medical center
+            //a medical center object used to delete a medical center
             MedicalCenter medicalCenter = new MedicalCenter();
-            //returns all medical center ids in an array
-            return medicalCenter.displayAllHRIDs();
+            //delete the medical center
+            medicalCenter.delete(hrId);
+            endFrame();
         }
-        else if(resource.equals("Water")){
-            //water object used to get the ids
-            Water water = new Water();
-            //returns all water ids in an array
-            return water.displayAllHRIDs();
-        }
-        else{
-            //food object used to get the ids
+        else if (resource.equals("Food")) {
+            //user wants to delete a food item
+            //a food object that is used to delete a food object
             Food food = new Food();
-            //returns all the food ids in an array
-            return food.displayAllHRIDs();
+            //delete the food object
+            food.delete(hrId);
+            endFrame();
+        }
+        else {
+            //user wants to delete water
+            //a water object that is used to delete a water
+            Water water = new Water();
+            //delete the water
+            water.delete(hrId);
+            //display the ending frame
+            endFrame();
         }
     }
+
 
     /**
      * Create the frame that the user sees when their operation has ended
@@ -689,5 +667,47 @@ public class CreateWindow {
         endFrame.getContentPane().add(BorderLayout.NORTH, label);
         endFrame.getContentPane().add(BorderLayout.CENTER, panel);
         endFrame.setVisible(true);
+    }
+
+    /**
+     * A helper method to change a ' to a '' so there isn't a DB error
+     * @param values the values to be checked
+     * @return an array that has strings that can be inserted into the DB
+     */
+    private static String[] checkForApostrophes(String[] values){
+        //loop through array
+        for(int i = 0; i < values.length; i++) {
+            //replace ' with ''
+            values[i] = values[i].replace("'", "''");
+        }
+        return values;
+    }
+
+    /**
+     * A helper method to get an array of all the ids for a certain resource.
+     * This is useful because it is used to allow users to only select and id
+     * from the resource that they selected
+     * @param resource the resource the user selected
+     * @return an array of strings with the ids
+     */
+    public static String[] getIDArray(String resource) {
+        if(resource.equals("Medical Center")){
+            //medical center object used to get the ids
+            MedicalCenter medicalCenter = new MedicalCenter();
+            //returns all medical center ids in an array
+            return medicalCenter.displayAllHRIDs();
+        }
+        else if(resource.equals("Water")){
+            //water object used to get the ids
+            Water water = new Water();
+            //returns all water ids in an array
+            return water.displayAllHRIDs();
+        }
+        else{
+            //food object used to get the ids
+            Food food = new Food();
+            //returns all the food ids in an array
+            return food.displayAllHRIDs();
+        }
     }
 }
